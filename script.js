8484cef96681237b17bb900d5fb2ba634bef2f88
jessiebeaver <59899303+jessiebeaver@users.noreply.github.com>
2020-01-29 21:01:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = prompt("What is your first name?");
+var color = prompt("What is your favorite color?")
+
+document.body.innerHTML = "My name is "+name+" and my favorite color is "+color;
