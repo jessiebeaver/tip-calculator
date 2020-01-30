ba61bce05603a4831c89fc89fbad9fa877b57d42
jessiebeaver <59899303+jessiebeaver@users.noreply.github.com>
2020-01-29 21:04:32 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-var name = prompt("What is your first name?");
-var color = prompt("What is your favorite color?")
-
-document.body.innerHTML = "My name is "+name+" and my favorite color is "+color;
